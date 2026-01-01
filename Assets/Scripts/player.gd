d3449e1f39ce013a1b23cede639b98dbc379e15b
crazyorange49 <84763581+crazyorange49@users.noreply.github.com>
2026-01-01 16:36:21 -0600

    update: Add player movement and camera to scene

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -1,1 +1,9 @@
 extends CharacterBody2D
+
+var speed: float = 400
+var rotationSpeed: float = 100
+
+func _physics_process(delta: float) -> void:
+	var moveInput = Input.get_vector("left","right", "up","down")
+	velocity = moveInput * speed
+	move_and_slide()
