f528f2936441a675e203fae7fe42941866a54305
Mason Miller <84522938+Mason534@users.noreply.github.com>
2026-01-02 17:20:48 -0600

    sdg

--- Assets/Scripts/enemySlime.gd
+++ Assets/Scripts/enemySlime.gd
@@ -20,6 +20,6 @@ func _on_detecion_area_body_entered(body: Node2D) -> void:
 	playerChase = true
 
 
-func _on_detecion_area_body_exited(body: Node2D) -> void:
+func _on_detecion_area_body_exited(_body: Node2D) -> void:
 	victim = null
 	playerChase = false
--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var speed: float = 400
 var rotationSpeed: float = 100
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var moveInput = Input.get_vector("left","right", "up","down")
 	velocity = moveInput * speed
 	move_and_slide()
