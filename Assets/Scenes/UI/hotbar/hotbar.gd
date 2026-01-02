4f5d17b340fd3baa76fe42bec83d213b581c6271
crazyorange49 <84763581+crazyorange49@users.noreply.github.com>
2026-01-02 00:39:18 -0600

    fix: Add label to hotbar and update selection logic

--- Assets/Scenes/UI/hotbar/hotbar.gd
+++ Assets/Scenes/UI/hotbar/hotbar.gd
@@ -1,7 +1,17 @@
 extends Control
 
-var currentSelection : int = 0
+var currentSelection : float = 1
+@onready var label: Label = $Label
 
 func _input(event: InputEvent) -> void:
-	if event.is_action("hotbarMove"):
-		pass
+	if event.is_action("hotbarMoveLeft") or event.is_action("hotbarMoveRight"):
+		currentSelection = clamp(currentSelection + .5 if event.is_action("hotbarMoveLeft") else currentSelection - .5, 1, 9)
+		update_hotbar()
+		
+func hotbar_Button_Pressed(button) -> void:
+	currentSelection = int()
+	update_hotbar()
+
+
+func update_hotbar():
+	label.text = str(currentSelection)
