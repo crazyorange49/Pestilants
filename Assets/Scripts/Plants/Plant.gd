6a504c04d4e26a2c92a11d1f56cb8117ed1fe702
crazyorange49 <84763581+crazyorange49@users.noreply.github.com>
2026-01-04 14:21:49 -0600

    update: added base code for all plants

--- Assets/Scripts/Plants/Plant.gd
+++ Assets/Scripts/Plants/Plant.gd
@@ -2,6 +2,18 @@ class_name Plant
 extends Node2D
 
 @export var stats: Resource = preload("uid://csr8dxvj1ns2l")
+@export var health: int
+@export var atkDamage: int
+@export var atkCoolDownInSeconds: float
+@export var visionRadius: float
+@export var movementSpeed: float
+
+func _init(p_health: int, p_atkDamage: int, p_atkCoolDownInSeconds: float, p_visionRadius: float, p_movementSpeed: float) -> void:
+	health = p_health
+	atkDamage = p_atkDamage
+	atkCoolDownInSeconds  = p_atkCoolDownInSeconds
+	visionRadius  = p_visionRadius
+	movementSpeed  = p_movementSpeed
 
 var icon : Texture = stats.icon:
 	set(icon):
