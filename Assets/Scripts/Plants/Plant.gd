b3b72672e9fe62bd282f35ecc5ed03df888a7bef
crazyorange49 <84763581+crazyorange49@users.noreply.github.com>
2026-01-04 20:26:53 -0600

    update: Add Bravestem and Sunpedal plant scenes and scripts

--- Assets/Scripts/Plants/Plant.gd
+++ Assets/Scripts/Plants/Plant.gd
@@ -1,19 +1,32 @@
 class_name Plant
 extends Node2D
 
+static var group: StringName = "plant"
 @export var stats: Resource = preload("uid://csr8dxvj1ns2l")
-@export var health: int
 @export var atkDamage: int
+@export var maxHealth: int
 @export var atkCoolDownInSeconds: float
 @export var visionRadius: float
 @export var movementSpeed: float
+@export var atkRange: float
 
-func _init(p_health: int = 0, p_atkDamage: int = 0, p_atkCoolDownInSeconds: float = 0.0, p_visionRadius: float = 0.0, p_movementSpeed: float = 0.0) -> void:
-	health = p_health
+
+@onready var visionCollisionBox: CollisionShape2D = $VisionArea/Radius
+@onready var attackRangeCollisionBox: CollisionShape2D = $AttackArea/Range
+
+var dayTimePos: Vector2
+
+func _init(p_maxHealth: int = 0, p_atkDamage: int = 0, p_atkCoolDownInSeconds: float = 0.0, p_visionRadius: float = 0.0, p_movementSpeed: float = 0.0, p_atkRange: float = 0.0) -> void:
+	maxHealth = p_maxHealth
 	atkDamage = p_atkDamage
 	atkCoolDownInSeconds  = p_atkCoolDownInSeconds
 	visionRadius  = p_visionRadius
 	movementSpeed  = p_movementSpeed
+	atkRange = p_atkRange
+
+func _ready() -> void:
+	visionCollisionBox.shape.radius = visionRadius
+	attackRangeCollisionBox.shape.radius = atkRange
 
 var icon : Texture = stats.icon:
 	set(icon):
@@ -32,3 +45,12 @@ var itemName: StringName = stats.itemName:
 		stats.itemName = name
 	get:
 		return itemName
+
+var health: int:
+	set(newHealth):
+		health = clamp(newHealth, 0, maxHealth) 
+	get:
+		return health
+		
+func subtractDamage(damage: int) -> void:
+	health -= damage
---
+++ Assets/Scripts/Plants/Sunpedal/sunpedal.gd
@@ -0,0 +1,2 @@
+class_name Sunpedal
+extends Plant
