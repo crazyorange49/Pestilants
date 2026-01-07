2566c46914faea93b2d0a260763295989a204fce
Mason Miller <84522938+Mason534@users.noreply.github.com>
2026-01-06 23:53:54 -0600

    shop almost done

--- Assets/Scenes/UI/Shop/shopMenu.gd
+++ Assets/Scenes/UI/Shop/shopMenu.gd
@@ -8,6 +8,12 @@ var Items: Array[itemStats]
 @onready var currency_label: Label = %currencyLabel
 @onready var player: CharacterBody2D = $"../Player"
 
+@onready var item_image: TextureRect = %itemImage
+@onready var item_name: Label = %itemName
+@onready var item_description: Label = %itemDescription
+@onready var item_price: Label = %itemPrice
+
+
 
 @onready var currency = player.getRenewalSeedCount()
 
@@ -29,7 +35,19 @@ func populatePlantList(plants : Array[itemStats]) -> void:
 		var shop_plant : ShopItemButton = SHOP_ITEM_BUTTON.instantiate()
 		shop_plant.setup_item( plant )
 		shop_items_container.add_child( shop_plant )
+		shop_plant.focus_entered.connect( updateItemDetails.bind( plant ) )
 		#connect to signals
 		pass
 	pass
 	
+func focusedItemChanged(plant : itemStats) -> void:
+	if plant:
+		updateItemDetails( plant )
+	pass
+	
+func updateItemDetails(plant : itemStats) -> void:
+	item_image.texture = plant.icon
+	item_name.text = plant.itemName
+	item_description.text = plant.description
+	item_price.text = str(plant.price)
+	pass
