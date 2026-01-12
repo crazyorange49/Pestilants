2eba21f699f687a5d223ecf90f9a25b49be7ee04
Mason Miller <84522938+Mason534@users.noreply.github.com>
2026-01-11 19:42:01 -0600

    Idk what this is

--- Assets/Scenes/DefenseItems/moonlight_reflector.gd
+++ Assets/Scenes/DefenseItems/moonlight_reflector.gd
@@ -8,8 +8,9 @@ func _on_vision_area_body_exited(_body: Node2D) -> void:
 	map.hud.tooltip.visible = false
 	pass
 
-func _process(float) -> void:
+func _process(_float) -> void:
 	if(map.nightEnded == true):
 		point_light_2d.visible = false
 	else:
 		point_light_2d.visible = true
+ 
