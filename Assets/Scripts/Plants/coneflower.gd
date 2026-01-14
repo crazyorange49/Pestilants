83b1b00efb3e7445236ca24b5295469fd2f4e64e
Mason Miller <84522938+Mason534@users.noreply.github.com>
2026-01-14 01:02:38 -0600

    I work with dummies x2

--- Assets/Scripts/Plants/coneflower.gd
+++ Assets/Scripts/Plants/coneflower.gd
@@ -1,10 +1,19 @@
 class_name Coneflower
 extends Plant
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $PointLight2D/AnimationPlayer
+var makeAidenMad = 0
+
 func _ready() -> void:
 	super._ready()
 	health = maxHealth
 
+func _process(delta: float) -> void:
+	if( map.nightEnded == true ):
+		animated_sprite_2d.play("dayIdle")
+	else:
+		animated_sprite_2d.play("nightIdle")
 
 func _on_vision_area_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
@@ -37,6 +46,9 @@ func getHealTarget():
 		return bestTarget
 	else:
 		return null
+
+#animation_player.play("healOff")  Use these when target is found and when target is null
+#animation_player.play("healOn")
 
 func _on_nav_timer_timeout() -> void:
 	getNewPosition()
