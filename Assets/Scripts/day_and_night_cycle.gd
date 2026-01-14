b29ea2c111ff3d0ddc8f1d1ad00cf1374a284fd6
Mason Miller <84522938+Mason534@users.noreply.github.com>
2026-01-14 03:24:04 -0600

    MUSIC

--- Assets/Scripts/day_and_night_cycle.gd
+++ Assets/Scripts/day_and_night_cycle.gd
@@ -3,6 +3,8 @@ class_name DayAndNightCycle
 
 signal changeDayTime(dayTime: DAY_STATE)
 
+@onready var dayMusic: AudioStreamPlayer = $AudioStreamPlayer
+@onready var nightMusic: AudioStreamPlayer = $AudioStreamPlayer2
 @onready var map: Node2D = $"../Map"
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var player: CharacterBody2D = $"../Player"
@@ -12,12 +14,16 @@ var dayTime : DAY_STATE = DAY_STATE.NOON
 
 func _ready() -> void:
 	add_to_group("dayAndNightCycle")
+	dayMusic.play()
 	pass
 	
 func _on_timer_timeout() -> void:
 	if dayTime != DAY_STATE.EVENING:
 		dayTime = DAY_STATE.EVENING
 		map.changeNight()
+		if(nightMusic.playing == false):
+			dayMusic.stop()
+			nightMusic.play()
 		player.light.visible = true
 		player.lightAni.play("lightOn")   
 		SignalBus.emit_signal("NightTime")
@@ -26,6 +32,9 @@ func _on_timer_timeout() -> void:
 		print("nightTIME!!")
 	elif dayTime != DAY_STATE.NOON:
 		SignalBus.emit_signal("DayTime")
+		if(dayMusic.playing == false):
+			nightMusic.stop()
+			dayMusic.play()
 		dayTime = DAY_STATE.NOON
 		changeDayTime.emit(dayTime)
 		player.lightAni.play("lightOff")  
