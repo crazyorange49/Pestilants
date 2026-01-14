c9eff8e3b01aaf067ed5f67ee4aaeec729753a34
crazyorange49 <84763581+crazyorange49@users.noreply.github.com>
2026-01-13 23:44:39 -0600

    kill me

--- Assets/Scenes/DefenseItems/decoy_sprout.gd
+++ Assets/Scenes/DefenseItems/decoy_sprout.gd
@@ -4,19 +4,13 @@ class_name DecoySprout
 @onready var timer: Timer = $"../../../dayAndNight/Timer"
 
 func _ready():
-	pass
-
-func Damage():
-	health -= 20
-	if(health <= 0):
-		queue_free()
+	health = maxHealth
 
 func _process(_float) -> void:
 	pass
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	pass
 
-func _on_vision_area_body_exited(body: Node2D) -> void:
+func _on_vision_area_body_exited(_body: Node2D) -> void:
 	map.hud.tooltip.visible = false
-	pass # Replace with function body.
