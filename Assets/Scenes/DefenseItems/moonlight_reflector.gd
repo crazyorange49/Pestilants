c0d7c1f3510479466a786c06ac59fde0fa7f65bb
Boomerdoom03 <kaleb.i.meissner-1@ou.edu>
2026-01-14 05:50:26 -0600

    Moon Ref;lector change, WIP?

--- Assets/Scenes/DefenseItems/moonlight_reflector.gd
+++ Assets/Scenes/DefenseItems/moonlight_reflector.gd
@@ -2,12 +2,13 @@ extends CharacterBody2D
 class_name MoonlightReflector
 
 @export var maxHealth: int = 100
+@export var damage = 5
 @export var health: int  = 100
 @export var minHealth: int = 0
 @onready var map: Map = $"../../"
 @onready var point_light_2d: PointLight2D = $PointLight2D
-@onready var timer: Timer = $"../../../dayAndNight/Timer"
-
+@onready var damage_timer: Timer = $DamageTimer
+var enemies_in_light: Array[Enemy] = []
 func _ready():
 	SignalBus.connect("DayTime", Callable(self, "Damage") )
 	pass
@@ -24,5 +25,33 @@ func _on_vision_area_body_exited(_body: Node2D) -> void:
 func _process(_float) -> void:
 	if(map.nightEnded == true and health > 0):
 		point_light_2d.visible = false
+		damage_timer.stop()
 	else:
 		point_light_2d.visible = true
+		if enemies_in_light.size() > 0 and damage_timer.is_stopped():
+			damage_timer.start()
+
+
+
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	enemies_in_light.append(body)
+	if not map.nightEnded:
+		damage_timer.start()
+	for enemy in enemies_in_light:
+		print(enemy.name)
+		
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+		enemies_in_light.erase(body)
+		if enemies_in_light.is_empty():
+			damage_timer.stop()
+		print("exit")
+			
+			
+func _on_damage_timer_timeout() -> void:
+	for enemy in enemies_in_light:
+		if is_instance_valid(enemy):
+			enemy.subtractDamage(damage)
